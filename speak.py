9447857e0e2836bebf23056029ddf4b18494291b
vava <vavalol886@gmail.com>
2023-07-10 23:26:08 +0300

    speech to text (по возможности заменить на быстрее )

--- speak.py
+++ speak.py
@@ -10,6 +10,7 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
+
 def listen():
     while True:
         data = stream.read(4096, exception_on_overflow=False)
