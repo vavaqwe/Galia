267e756bce2a486a1065b7103e9c37552feb04e6
vava <vavalol886@gmail.com>
2023-07-12 18:34:13 +0300

    сделал многопоточность и код с треем и гуи

--- main.py
+++ main.py
@@ -1,30 +1,80 @@
-import tkinter as tk
-import asyncio
-import asynctkinter as at
-
-from speak import listen
+# C ТРЕЕМ
+from  speak import *
 from voice import va_speak
+import sys
+import threading
+import pystray
+from pystray import MenuItem as item
+from PIL import Image
 
-at.patch_unbind()
 
-async def main():
-    async for text in listen():
-        process_text(text)
-        label.config(text=text)
-        await asyncio.sleep(0.1)
 
-def process_text(text):
-    print(text)
+def listen():
+    while True:
+        data = stream.read(4096, exception_on_overflow=False)
+        if (rec.AcceptWaveform(data)) and len(data) > 0:
+            answer = json.loads(rec.Result())
+            if answer['text']:
+                on_startup(answer['text'])
 
-async def listen_wrapper():
-    async for text in listen():
-        yield text
+def start_listening():
+    thread = threading.Thread(target=listen)
+    thread.daemon = True
+    thread.start()
+
+
+def on_startup(text: str):
+    va_speak(text)
+
+def on_exit():
+    print("Приложение завершено")
+    tray.stop()
+    sys.exit(0)
+
+
+menu = (
+    item('Выход', on_exit),
+)
+
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    label = tk.Label(root, text="Ожидание результата...")
-    label.pack()
-    print('lol')
-    at.start(main())
-    root.mainloop()
+    image = Image.open('ico/gpng.png')
+    tray = pystray.Icon("name", image, "Заголовок", menu)
+    start_listening()
+    tray.run()
 
+
+
+
+# С ИНТЕРФейСОМ
+# from  speak import *
+# from voice import va_speak
+#
+# import tkinter as tk
+# import threading
+#
+#
+#
+# def listen():
+#     while True:
+#         data = stream.read(4096, exception_on_overflow=False)
+#         if (rec.AcceptWaveform(data)) and len(data) > 0:
+#             answer = json.loads(rec.Result())
+#             if answer['text']:
+#                 on_startup(answer['text'])
+#
+# def start_listening():
+#     thread = threading.Thread(target=listen)
+#     thread.daemon = True
+#     thread.start()
+#
+#
+# def on_startup(text: str):
+#     va_speak(text)
+#
+#
+# if __name__ == "__main__":
+#     start_listening()
+#     root = tk.Tk()
+#
+#     root.mainloop()
