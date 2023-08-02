a3237c0c8b62321694171ad4842d850cc57f9787
vava <vavalol886@gmail.com>
2023-08-02 20:47:42 +0300

    задание

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
-responses = {
-    "привет": "Привет! Чем я могу помочь?",
-    "как дела": "У меня всё отлично, спасибо!",
-    "погода": "Извините, я пока не умею предоставлять информацию о погоде.",
-    # Добавьте здесь другие ключевые фразы и соответствующие ответы
-}
-
-def get_response(query):
-    for key in responses:
-        if key in query:
-            return responses[key]
-    return "Извините, не могу понять ваш запрос."
-
-print(get_response("привет"))
+# responses = {
+#     "привет": "Привет! Чем я могу помочь?",
+#     "как дела": "У меня всё отлично, спасибо!",
+#     "погода": "Извините, я пока не умею предоставлять информацию о погоде.",
+#     # Добавьте здесь другие ключевые фразы и соответствующие ответы
+# }
+#
+# def get_response(query):
+#     for key in responses:
+#         if key in query:
+#             return responses[key]
+#     return "Извините, не могу понять ваш запрос."
+#
+# print(get_response("привет"))
+from googlesearch import search
+for i in search('python'):
+    print(i)
+    break
